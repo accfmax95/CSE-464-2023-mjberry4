61490a17f15d49f6e0fb849f90b685f849f8cf0c
accfmac95 <mjberry4@asu.edu>
2023-04-20 22:14:55 -0700

    Implementation of the template pattern design

--- src/test/java/GraphManagerTest.java
+++ src/test/java/GraphManagerTest.java
@@ -145,17 +145,19 @@ public class GraphManagerTest {
     public void testGraphSearch() {
 
         try {
+            BFS bfs = new BFS();
+            DFS dfs = new DFS();
             g.parseGraph("src/input.dot");
 
             MutableNode src = g.graph.nodes().stream().filter(node -> node.name().toString().equals("a")).findFirst().orElse(null);
             MutableNode dst = g.graph.nodes().stream().filter(node -> node.name().toString().equals("k")).findFirst().orElse(null);
 
-            String bfsPath = g.GraphSearch(src, dst, GraphManager.Algorithm.BFS);
+            String bfsPath = bfs.GraphSearch(src, dst, g.graph);
 
             Assert.assertEquals("a -> b -> c -> h -> j -> k", bfsPath);
             System.out.println("BFS Output: " + bfsPath);
 
-            String dfsPath = g.GraphSearch(src, dst, GraphManager.Algorithm.DFS);
+            String dfsPath = dfs.GraphSearch(src, dst, g.graph);
 
             Assert.assertEquals("a -> b -> c -> h -> i -> k", dfsPath);
             System.out.println("DFS Output: " + dfsPath);
@@ -163,7 +165,7 @@ public class GraphManagerTest {
             src = g.graph.nodes().stream().filter(node -> node.name().toString().equals("a")).findFirst().orElse(null);
             dst = g.graph.nodes().stream().filter(node -> node.name().toString().equals("d")).findFirst().orElse(null);
 
-            bfsPath = g.GraphSearch(src, dst, GraphManager.Algorithm.BFS);
+            bfsPath = bfs.GraphSearch(src, dst, g.graph);
 
             Assert.assertEquals("a -> b -> c -> d", bfsPath);
             System.out.println("BFS Output: " + bfsPath);
@@ -172,7 +174,7 @@ public class GraphManagerTest {
             dst = g.graph.nodes().stream().filter(node -> node.name().toString().equals("i")).findFirst().orElse(null);
 
 
-            dfsPath = g.GraphSearch(src, dst, GraphManager.Algorithm.DFS);
+            dfsPath = dfs.GraphSearch(src, dst, g.graph);
 
             Assert.assertEquals("e -> f -> g -> h -> i", dfsPath);
             System.out.println("DFS Output: " + dfsPath);
